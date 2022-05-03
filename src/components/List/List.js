f5af376e23c6d73537c69c28ad19afddcc414c49
caroline <ambar.caroline@gmail.com>
2022-05-03 10:37:49 -0300

    Inclua a funcionalidade de adicionar novos itens na lista com o form

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Item from "../Item/Item";
 
-const List = () => (
-    <ul data-ul>
-        <Item value="Tarefa 1" id="1"/>
+const List = ({items}) => (
+    <ul>
+        {
+            items.map((item) => (
+                <Item value={item.value} id={item.id}/>
+            ))
+        }
     </ul>
 )
 
