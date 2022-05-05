756962659264f8426603c1e3897a0808cd4a5544
caroline <ambar.caroline@gmail.com>
2022-05-05 09:21:34 -0300

    Inclua a funcionalidade de editar o Título

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,8 +1,11 @@
 import React from "react";
 import './Title.css';
 
-const Title = () => (
-    <input type="text" value="My List" placeholder="My List" class="title"/>
-)
+const Title = () => {
+    const [value, setValue] = React.useState('My List')
+    return (
+        <input type="text" value={value} onChange={(evento) => setValue(evento.target.value)} placeholder="My List" class="title"/>
+    )
+}
 
 export default Title
